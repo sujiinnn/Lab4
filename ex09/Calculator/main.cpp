00a57de6f6dfce37878ec04153120dd9934c7d88
sujiinnn <sujiinnn@gmail.com>
2023-12-12 00:19:43 +0900

    ex09 Qt를 이용한 계산기 프로그램

---
+++ ex09/Calculator/main.cpp
@@ -0,0 +1,9 @@
+#include "calculator.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Calculator calculator;
+    calculator.show();
+    return app.exec();
+}
